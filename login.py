837daff8b0f4697cf99c9338b5543a47078398a6
Eugene Teu Chu Wei <eugeneteu@r-250-104-25-172.comp.nus.edu.sg>
2019-03-04 14:48:46 +0800

    Switched username and password to be taken in by input

--- login.py
+++ login.py
@@ -3,6 +3,8 @@ import requests
 from lxml import html
 from bs4 import BeautifulSoup
 
+username = input("enter your username: ")
+password = input("enter your password: ")
 
 #url = 'https://luminus.nus.edu.sg/v2/auth/connect/authorize?client_id=verso&amp;redirect_uri=https%3A%2F%2Fluminus.nus.edu.sg%2Fauth%2Fcallback&amp;response_type=id_token%20token%20code&amp;scope=profile%20email%20role%20openid%20lms.read%20calendar.read%20lms.delete%20lms.write%20calendar.write%20gradebook.write%20offline_access&amp;state=cc64574ce9734e5a8c1f357f4e030f18&amp;nonce=783f5ae1c70c43e2b5f13c2b0fadeed3'
 url = 'https://luminus.nus.edu.sg/.php'
@@ -17,8 +19,8 @@ with requests.Session() as s:
     
     payload = {
     #'idsrv.xsrf': authenticity_token,
-    'username': '',
-    'password': ''
+    'username': username,
+    'password': password
     }
 
     
