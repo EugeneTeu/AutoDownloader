47a62f81db14e3d418c27d4f7fd47c50cd9b8616
Eugene Teu Chu Wei <eugeneteu@Eugenes-MacBook-Pro-4.local>
2019-03-15 01:10:02 +0800

    Played around with the html parser, learned functionality of soup.children

---
+++ scraper.py
@@ -0,0 +1,21 @@
+import requests
+
+from lxml import html
+from bs4 import BeautifulSoup
+
+
+url = "https://www.imdb.com/title/tt0109830/?ref_=nv_sr_1"
+#"https://www.imdb.com/title/tt5977276/?pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=2413b25e-e3f6-4229-9efd-599bb9ab1f97&pf_rd_r=ENDEWR95YZ20BTNAR1A3&pf_rd_s=right-2&pf_rd_t=15061&pf_rd_i=homepage&ref_=hm_otw_t4"
+
+with requests.Session() as s:
+    result = s.get(url)
+    soup = BeautifulSoup(result.content,'lxml')
+    #print(soup.prettify)
+    #t = soup.findAll('')
+    link = list(soup.children)
+    for item in link:
+        print(type(item))
+    print (link[1])
+
+
+
